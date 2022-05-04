7efbaaa1b3c84181222ab9a64ec790cd38f4ca81
Kevin Contreras <kvncont@gmail.com>
2022-05-03 23:29:32 -0600

    feat: add docstrings

--- app/routers/pet.py
+++ app/routers/pet.py
@@ -14,11 +14,13 @@ FIND_PET_BY_ID = "SELECT * FROM c WHERE c.id='{}'"
 
 @router.get("/")
 async def find_all_pets():
+    """Find all the pets in the database"""
     return list(container.read_all_items())
 
 
 @router.get("/{id}")
 async def find_pet_by_id(id: str):
+    """Find a pet by its id"""
     for pet in container.query_items(
             query=FIND_PET_BY_ID.format(id),
             enable_cross_partition_query=True):
@@ -27,6 +29,7 @@ async def find_pet_by_id(id: str):
 
 @router.put("/", status_code=201)
 async def upsert_pet(pet: Pet):
+    """Insert or update a pet if it doesn't exist by its id"""
     if pet.id == None:
         pet.id = str(uuid.uuid4())
     return container.upsert_item(pet.dict())
@@ -34,5 +37,6 @@ async def upsert_pet(pet: Pet):
 
 @router.delete("/{id}", status_code=204)
 async def delete_pet_by_id(id: str):
+    """Delete a pet by its id"""
     container.delete_item(id, partition_key=id)
     return Response(status_code=204)
